293f041da2d5f6455751b4fdfb21da7a70b3f7ba
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-07-20 02:28:18 -0300

    refactor: separate logic of node monitor

--- otp_supervisor/lib/otp_supervisor/application.ex
+++ otp_supervisor/lib/otp_supervisor/application.ex
@@ -23,24 +23,36 @@ defmodule OtpSupervisor.Application do
 
     supervisor_state = Supervisor.start_link(children, opts)
 
+    start_cache()
+    start_monitor()
+
+    supervisor_state
+  end
+
+  defp start_cache do
     :timer.sleep(:rand.uniform(1000))
     CacheSupervisor.start_child()
+  end
 
+  defp start_monitor do
     spawn fn ->
       :net_kernel.monitor_nodes(:true, %{ nodedown_reason: true })
       monitor()
     end
-    supervisor_state
   end
 
-  def monitor() do
+  defp monitor() do
     receive do
       {:nodedown, _, _} ->
-        :timer.sleep(:rand.uniform(1000))
-        if !is_pid(:global.whereis_name(Cache)), do:
-          CacheSupervisor.start_child()
+        handle_node()
       _ -> nil
     end
     monitor()
+  end
+
+  def handle_node do
+    :timer.sleep(:rand.uniform(1000))
+    if !is_pid(:global.whereis_name(Cache)), do:
+      CacheSupervisor.start_child()
   end
 end
