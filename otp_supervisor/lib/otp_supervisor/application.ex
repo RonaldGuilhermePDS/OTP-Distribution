d5cac2f2e36456e4985ce96db6d0febcffe188b0
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-07-20 01:58:01 -0300

    feat: add nodes monitor to start cache supervisor when cache supervisor node down

--- otp_supervisor/lib/otp_supervisor/application.ex
+++ otp_supervisor/lib/otp_supervisor/application.ex
@@ -1,33 +1,22 @@
 defmodule OtpSupervisor.Application do
+  alias Hex.Netrc.Cache
   use Application
 
   @impl true
   def start(_type, _args) do
-    { port, _} =
-      System.get_env("PORT", "4000")
-      |> Integer.parse()
+    port = System.get_env("PORT", "4000") |> String.to_integer()
 
     topologies = [
       OtpSupervisor: [
         strategy: Cluster.Strategy.LocalEpmd,
-        config: [hosts: [ "node@node_one,node@node_two" ]]
+        config: [hosts: ["node@node_one", "node@node_two"]]
       ]
     ]
 
     children = [
-      {
-        Cluster.Supervisor,
-        [topologies, [name: OtpSupervisor.ClusterSupervisor]]
-      },
-      {
-        CacheSupervisor, []
-      },
-      {
-        Bandit,
-        plug: OtpSupervisor.Router,
-        scheme: :http,
-        port: port
-      },
+      {Cluster.Supervisor, [topologies, [name: OtpSupervisor.ClusterSupervisor]]},
+      {CacheSupervisor, []},
+      {Bandit, plug: OtpSupervisor.Router, scheme: :http, port: port}
     ]
 
     opts = [strategy: :one_for_one, name: OtpSupervisor.Supervisor]
@@ -37,6 +26,21 @@ defmodule OtpSupervisor.Application do
     :timer.sleep(:rand.uniform(1000))
     CacheSupervisor.start_child()
 
+    spawn fn ->
+      :net_kernel.monitor_nodes(:true, %{ nodedown_reason: true })
+      monitor()
+    end
     supervisor_state
+  end
+
+  def monitor() do
+    receive do
+      {:nodedown, _, _} ->
+        :timer.sleep(:rand.uniform(1000))
+        if !is_pid(:global.whereis_name(Cache)), do:
+          CacheSupervisor.start_child()
+      _ -> nil
+    end
+    monitor()
   end
 end
